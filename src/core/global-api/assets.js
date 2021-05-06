b43ab8b5103632ebf71e7de5d03a9c03ab54ec16
xzp <xingzepeng@geo-compass.com>
2021-05-06 17:29:42 +0800

    fix(Vue初始化-静态成员): Vue初始化-静态成员

--- src/core/global-api/assets.js
+++ src/core/global-api/assets.js
@@ -7,6 +7,9 @@ export function initAssetRegisters (Vue: GlobalAPI) {
   /**
    * Create asset registration methods.
    */
+  // 遍历了ASSET_TYPES数组,为Vue定义相应方法
+  // ASSET_TYPES包括了directive\ component\ filter 
+  
   ASSET_TYPES.forEach(type => {
     Vue[type] = function (
       id: string,
@@ -19,13 +22,16 @@ export function initAssetRegisters (Vue: GlobalAPI) {
         if (process.env.NODE_ENV !== 'production' && type === 'component') {
           validateComponentName(id)
         }
+        // Vue.component('comp', {template: ''})
         if (type === 'component' && isPlainObject(definition)) {
           definition.name = definition.name || id
+          // 把组件配置转换为组件的构造函数
           definition = this.options._base.extend(definition)
         }
         if (type === 'directive' && typeof definition === 'function') {
           definition = { bind: definition, update: definition }
         }
+        // 全局注册，存储资源并赋值
         this.options[type + 's'][id] = definition
         return definition
       }
--- src/core/global-api/index.js
+++ src/core/global-api/index.js
@@ -52,7 +52,8 @@ export function initGlobalAPI (Vue: GlobalAPI) {
     observe(obj)
     return obj
   }
-
+// 初始化Vue.options对象,并给其扩展
+// component/directive/filter
   Vue.options = Object.create(null)
   ASSET_TYPES.forEach(type => {
     Vue.options[type + 's'] = Object.create(null)
@@ -61,11 +62,14 @@ export function initGlobalAPI (Vue: GlobalAPI) {
   // this is used to identify the "base" constructor to extend all plain-object
   // components with in Weex's multi-instance scenarios.
   Vue.options._base = Vue
-
+// 设置keep-alive组件
   extend(Vue.options.components, builtInComponents)
-
+// 注册Vue.use()用来注册插件
   initUse(Vue)
+  // 注册Vue.mixin()实现混入
   initMixin(Vue)
+  // 注册Vue.extend()基于传入的options返回一个组件的构造函数
   initExtend(Vue)
+  // 注册Vue.directive() Vue.component()  Vue.filter()
   initAssetRegisters(Vue)
 }
